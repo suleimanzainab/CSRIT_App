350d7c8ac0aeeba9eb6df0b2a0a64b06cf79e9ee
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-01-19 10:19:11 +0200

    Update settings.py

--- CSRIT_Project/settings.py
+++ CSRIT_Project/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
