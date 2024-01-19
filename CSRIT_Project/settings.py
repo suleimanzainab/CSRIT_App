28423d53b6c0e52666826c51cc6bd6f6ab6dd0dc
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-01-19 10:16:04 +0200

    Update settings.py

--- CSRIT_Project/settings.py
+++ CSRIT_Project/settings.py
@@ -182,12 +182,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     # ...
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
