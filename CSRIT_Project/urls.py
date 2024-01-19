e9944f6909cdb1f60d8f5f1af6f1ece52f0ceb99
suleimanzainab <157100053+suleimanzainab@users.noreply.github.com>
2024-01-19 11:28:10 +0200

    Update urls.py

--- CSRIT_Project/urls.py
+++ CSRIT_Project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
+from django.conf import settings
 from drf_yasg import openapi
 from django.conf.urls.static import static
 
